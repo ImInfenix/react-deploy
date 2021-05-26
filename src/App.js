8dbc8e9eea3f440158cdab779f1b80a4ce2bce49
ImInfenix <duncan.sourdin@pm.me>
2021-05-26 20:09:27 +0200

    Random modification

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Coucou !
+          Edit <code>src/App.js</code> and save to reload. Coucou ! Meh.
         </p>
         <a
           className="App-link"
