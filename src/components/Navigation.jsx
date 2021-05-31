e37436343282d41c3d3490d4213f2165da7f4677
ImInfenix <duncan.sourdin@pm.me>
2021-05-31 12:53:41 +0200

    Added section highlight

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
+var currentSection = window.location.href
+  .substring(window.location.href.lastIndexOf("/") + 1)
+  .toLowerCase();
+
+if (currentSection === "") currentSection = "home";
+
 class Navigation extends React.Component {
+  setCurrentSection(value) {
+    console.log("old " + currentSection);
+    currentSection = value;
+    console.log("new " + currentSection);
+  }
+
   render() {
     return (
       <nav>
@@ -14,22 +26,45 @@ class Navigation extends React.Component {
               />
             </Link>
           </li>
-          <li className="underline">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="underline">
-            <Link to="games">Games</Link>
-          </li>
-          <li className="underline">
-            <Link to="projects">Projects</Link>
-          </li>
-          <li className="underline">
-            <Link to="about">About</Link>
-          </li>
+          <NavigationItem
+            to="/"
+            displayText="Home"
+            onClick={this.setCurrentSection}
+          />
+          <NavigationItem
+            to="/games"
+            displayText="Games"
+            onClick={this.setCurrentSection}
+          />
+          <NavigationItem
+            to="/projects"
+            displayText="Projects"
+            onClick={this.setCurrentSection}
+          />
+          <NavigationItem
+            to="/about"
+            displayText="About"
+            onClick={this.setCurrentSection}
+          />
         </ul>
       </nav>
     );
   }
 }
+class NavigationItem extends React.Component {
+  render() {
+    var textLow = this.props.displayText.toLowerCase();
+    return (
+      <li className={currentSection === textLow ? "current" : "underline"}>
+        <Link
+          to={this.props.to}
+          onClick={this.props.onClick.bind(this, textLow)}
+        >
+          {this.props.displayText}
+        </Link>
+      </li>
+    );
+  }
+}
 
-export default withRouter(Navigation);
+export default withRouter(Navigation, NavigationItem);
